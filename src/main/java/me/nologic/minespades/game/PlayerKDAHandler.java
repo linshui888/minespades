c6c9913d4913ef1287fe5388466bec1c2a65b94e
NoLogic <pixel2374@gmail.com>
2023-04-26 10:47:27 +0600

    KDA fixes

--- src/main/java/me/nologic/minespades/game/PlayerKDAHandler.java
+++ src/main/java/me/nologic/minespades/game/PlayerKDAHandler.java
@@ -4,9 +4,7 @@ import me.nologic.minespades.game.event.BattlegroundPlayerDeathEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDamageEvent;
 
 /**
  * Специализированный класс, который крайне подробно обрабатывает событие смерти игрока.
@@ -21,19 +19,35 @@ public class PlayerKDAHandler {
 
         TextComponent textComponent;
         if (killer != null) {
+            String symbol = this.getDeathSymbol(event);
             textComponent = Component.text("")
-                    .color(TextColor.color(0xFFFFFF))
+                    .color(TextColor.color(0xC78C3B))
                     .append(victim.name().color(TextColor.fromHexString("#" + event.getVictim().getTeam().getColor())))
-                    .append(Component.text(" ⚔ ").decorate(TextDecoration.BOLD))
+                    .append(Component.text(symbol))
                     .append(killer.name().color(TextColor.fromHexString("#" + event.getKiller().getTeam().getColor())));
         } else {
-            textComponent = Component.text("")
-                    .color(TextColor.color(0xFFFFFF))
+            textComponent = Component.text("☠ ")
+                    .color(TextColor.color(0xC78C3B))
                     .append(victim.name().color(TextColor.fromHexString("#" + event.getVictim().getTeam().getColor())))
-                    .append(Component.text(" ☠").decorate(TextDecoration.BOLD));
+                    .append(Component.text(" ☠"));
         }
 
         event.getBattleground().getPlayers().forEach(battlegroundPlayer -> battlegroundPlayer.getPlayer().sendActionBar(textComponent));
+    }
+    
+    private String getDeathSymbol(BattlegroundPlayerDeathEvent event) {
+
+        if (event.getKiller() != null) {
+            String itemName = event.getKiller().getPlayer().getInventory().getItemInMainHand().getType().toString().toLowerCase();
+            if (itemName.contains("pickaxe")) {
+                return "⛏";
+            }
+        }
+
+        return switch (event.getDamageCause()) {
+            case PROJECTILE -> "➶";
+            default -> "⚔";
+        };
     }
 
 }
