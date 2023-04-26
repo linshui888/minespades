e7296e3131e7c55018f844bf5b597a2b6bafd304
NoLogic <pixel2374@gmail.com>
2023-04-26 10:50:12 +0600

    KDA fixes

--- src/main/java/me/nologic/minespades/game/PlayerKDAHandler.java
+++ src/main/java/me/nologic/minespades/game/PlayerKDAHandler.java
@@ -21,13 +21,13 @@ public class PlayerKDAHandler {
         if (killer != null) {
             String symbol = this.getDeathSymbol(event);
             textComponent = Component.text("")
-                    .color(TextColor.color(0xC78C3B))
+                    .color(TextColor.color(0x9B6433))
                     .append(victim.name().color(TextColor.fromHexString("#" + event.getVictim().getTeam().getColor())))
                     .append(Component.text(symbol))
                     .append(killer.name().color(TextColor.fromHexString("#" + event.getKiller().getTeam().getColor())));
         } else {
             textComponent = Component.text("☠ ")
-                    .color(TextColor.color(0xC78C3B))
+                    .color(TextColor.color(0x9B6433))
                     .append(victim.name().color(TextColor.fromHexString("#" + event.getVictim().getTeam().getColor())))
                     .append(Component.text(" ☠"));
         }
