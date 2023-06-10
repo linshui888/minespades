cda21667573236e8d04f7d7770f7f78f42123eeb
NoLogic <pixel2374@gmail.com>
2023-06-10 16:57:45 +0600

    Added Minority & language management

--- src/main/java/me/nologic/minespades/Minespades.java
+++ src/main/java/me/nologic/minespades/Minespades.java
@@ -7,14 +7,17 @@ import me.nologic.minespades.battleground.Multiground;
 import me.nologic.minespades.command.MinespadesCommand;
 import me.nologic.minespades.game.EventDrivenGameMaster;
 import me.nologic.minespades.util.MinespadesPlaceholderExpansion;
+import me.nologic.minority.MinorityExtension;
+import me.nologic.minority.MinorityFeature;
+import me.nologic.minority.annotations.Translatable;
+import me.nologic.minority.annotations.TranslationKey;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.util.Random;
 
-@Getter
-public final class Minespades extends JavaPlugin {
+@Getter @Translatable
+public final class Minespades extends MinorityExtension implements MinorityFeature {
 
     @Getter
     private static Minespades instance;
@@ -26,15 +29,26 @@ public final class Minespades extends JavaPlugin {
     private BattlegroundManager   battlegrounder;
     private PaperCommandManager   commandManager;
 
+    @TranslationKey(section = "log-info-messages", name = "battleground-folder-created", value = "Minespades created a battlegrounds folder.")
+    private String battlegroundFolderMessage;
+
+    @TranslationKey(section = "log-info-messages", name = "battleground-load", value = "Minespades is trying to load battleground %s.")
+    private String battlegroundLoadMessage;
+
+    @TranslationKey(section = "log-info-messages", name = "plugin-disable", value = "Minespades will be disabled. All running battlegrounds will be disabled.")
+    private String minespadesDisableMessage;
+
     @Override
     public void onEnable() {
         instance = this;
         random = new Random();
+        super.getConfigurationWizard().generate(this.getClass());
+        this.init(this, this.getClass(), this);
 
         File maps = new File(super.getDataFolder() + "/battlegrounds");
         if (!maps.exists()) {
             if (maps.mkdir()) {
-                getLogger().info("Minespades хранит карты в виде одной датабазы, см. папку battlegrounds.");
+                super.getLogger().info(battlegroundFolderMessage);
             }
         }
 
@@ -45,17 +59,16 @@ public final class Minespades extends JavaPlugin {
         getServer().getPluginManager().registerEvents(gameMaster, this);
         this.enableBattlegrounds();
 
-        // Поддержка для PAPI
         if (super.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null)
             new MinespadesPlaceholderExpansion().register();
     }
 
+    // TODO: Необходимо починить загрузку арен на старте.
     private void enableBattlegrounds() {
-        Bukkit.getLogger().info("Minespades пытается автоматически загрузить сохранённые арены.");
-        getConfig().getStringList("Battlegrounds").forEach(name -> {
+        super.getConfig().getStringList("battlegrounds").forEach(name -> {
 
             // FIXME: почини меня, дай мне свежесть
-            getLogger().info("Плагин пытается загрузить " + name + "...");
+            super.getLogger().info(String.format(battlegroundLoadMessage, name));
             battlegrounder.enable(name);
         });
     }
@@ -63,7 +76,7 @@ public final class Minespades extends JavaPlugin {
     @Override
     public void onDisable() {
 
-        this.getLogger().info("Minespades отключается, все работающие арены будут остановлены.");
+        super.getLogger().info(minespadesDisableMessage);
 
         for (Multiground multiground : battlegrounder.getMultigrounds()) {
             battlegrounder.disable(multiground.getBattleground());
@@ -72,6 +85,7 @@ public final class Minespades extends JavaPlugin {
         for (Battleground battleground : battlegrounder.getLoadedBattlegrounds()) {
             battlegrounder.disable(battleground);
         }
+
     }
 
 }
